6d3e35d0e645bc4343852604f01f16da7bb19228
Md Panna <75739631+mdpanna@users.noreply.github.com>
2023-12-23 21:29:06 +0600

    Create program.py

---
+++ program.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+source_folder = "path/to/your/source/folder"  # Replace with the actual path to your source folder
+
+destination_folders = {
+    "images": "path/to/images/folder",
+    "pdfs": "path/to/pdfs/folder",
+    "videos": "path/to/videos/folder"
+}
+
+for folder_name, destination_folder in destination_folders.items():
+    os.makedirs(destination_folder, exist_ok=True)  # Create folders if they don't exist
+
+for filename in os.listdir(source_folder):
+    file_extension = os.path.splitext(filename)[1].lower()
+    if file_extension in [".jpg", ".jpeg", ".png", ".gif"]:
+        destination = destination_folders["images"]
+    elif file_extension == ".pdf":
+        destination = destination_folders["pdfs"]
+    elif file_extension in [".mp4", ".mov", ".avi", ".wmv"]:
+        destination = destination_folders["videos"]
+    else:
+        continue  # Skip other file types
+
+    source_file = os.path.join(source_folder, filename)
+    destination_file = os.path.join(destination, filename)
+    shutil.move(source_file, destination_file)
+
+print("Files separated successfully!")
